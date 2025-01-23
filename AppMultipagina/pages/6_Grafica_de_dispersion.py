48b9409febb193caf626553ea5ea90f6d3c43a91
ValeriaPeniche <122946520+ValeriaPeniche@users.noreply.github.com>
2025-01-23 13:22:02 -0500

    Update 6_Grafica_de_dispersion.py

--- AppMultipagina/pages/6_Grafica_de_dispersion.py
+++ AppMultipagina/pages/6_Grafica_de_dispersion.py
@@ -3,11 +3,8 @@ import altair as alt
 import pandas as pd
 import numpy as np
 
+st.subheader("Gráfica de dispersión")
 df = pd.read_csv("../Datos/quality_life_2020.csv", sep=";")
-
-###################################
-# Gráfica de dispersión
-##################################
 
 # Gráfica de dispersión
 
@@ -17,7 +14,6 @@ variables_numericas = df.select_dtypes(include=['float64', 'int64']).columns.tol
 var_1 = st.sidebar.selectbox("Selecciona variable 1 (horizontal)", variables_numericas)
 var_2 = st.sidebar.selectbox("Selecciona variable 2 (vertical)", variables_numericas)
 
-st.subheader("Gráfica de dispersión")
 scatter_chart = alt.Chart(df).mark_circle().encode(
     x = alt.X(var_1, scale=alt.Scale(zero=False)),
     y = alt.Y(var_2, scale=alt.Scale(zero=False)),
@@ -27,3 +23,5 @@ scatter_chart = alt.Chart(df).mark_circle().encode(
 st.altair_chart(scatter_chart, use_container_width=True)
 
 st.write(f'Correlación entre {var_1} y {var_2}: {np.round(df[var_1].corr(df[var_2]), 2)}')
+
+st.page_link("pages/6_Grafica_de_dispersion.py", label="⬅️ Regresar")
