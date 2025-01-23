1cd90da6a07274e05f0d5b45dceba8da6e194eab
ValeriaPeniche <122946520+ValeriaPeniche@users.noreply.github.com>
2025-01-23 13:21:00 -0500

    Update 5_Grafica_de_caja.py

--- AppMultipagina/pages/5_Grafica_de_caja.py
+++ AppMultipagina/pages/5_Grafica_de_caja.py
@@ -2,18 +2,13 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 
+st.subheader("Gráfico de caja y bigotes")
 df = pd.read_csv("../Datos/quality_life_2020.csv", sep=";")
-
-###################################
-# Gráfica de caja y bigotes
-##################################
 
 # Selecciona la variabe a desplegar:
 # Crear un selector en la barra lateral para elegir la variable numérica a visualizar
 variables_numericas = df.select_dtypes(include=['float64', 'int64']).columns.tolist()  # Filtrar solo las variables numéricas
 variable_seleccionada = st.sidebar.selectbox("Selecciona la variable para la gráfica de caja", variables_numericas)
-
-st.subheader("Gráfico de caja y bigotes")
 
 st.write(variable_seleccionada)
 
@@ -25,4 +20,7 @@ box_plot = alt.Chart(df).mark_boxplot().encode(
 )
 
 st.altair_chart(box_plot, use_container_width=True)
+
+st.page_link("pages/4_Histograma.py", label="⬅️ Regresar")
+st.page_link("pages/6_Grafica_de_dispersion.py", label="Siguiente ➡️")
 
