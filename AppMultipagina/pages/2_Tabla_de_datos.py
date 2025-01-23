cbb0f5e81d8586d3ee46b0134f469d4090575dc2
ValeriaPeniche <122946520+ValeriaPeniche@users.noreply.github.com>
2025-01-23 00:58:24 -0500

    Update 2_Tabla_de_datos.py

--- AppMultipagina/pages/2_Tabla_de_datos.py
+++ AppMultipagina/pages/2_Tabla_de_datos.py
@@ -1,13 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-###################################
-# Tabla de datos
-##################################
-
 st.subheader("Tabla de datos")
 st.write("Los datos que exploraremos están disponibles en la siguiente tabla:")
-
-df = pd.read_csv("../Datos/quality_life_2020.csv", sep=";")
+df = pd.read_csv("Datos/quality_life_2020.csv", sep=";")
 st.dataframe(df)
 
+st.page_link("pages/1_Indicadores_calidad_de_vida.py", label="⬅️ Regresar a Inicio")
+st.page_link("pages/3_Grafica_de_barras.py", label="Siguiente ➡️")
