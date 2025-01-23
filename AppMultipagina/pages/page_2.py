468e97618b5a86e0b3c5e150b6906660f78bb574
ValeriaPeniche <122946520+ValeriaPeniche@users.noreply.github.com>
2025-01-23 14:14:04 -0500

    Update page_2.py

--- AppMultipagina/pages/page_2.py
+++ AppMultipagina/pages/page_2.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 st.subheader("Tabla de datos")
 st.write("Los datos que exploraremos están disponibles en la siguiente tabla:")
-df = pd.read_csv("Datos/quality_life_2020.csv", sep=";")
+df = pd.read_csv("AppMultipagina/Datos/quality_life_2020.csv", sep=";")
 st.dataframe(df)
 
-st.page_link("pages/1_Indicadores_calidad_de_vida.py", label="⬅️ Regresar a Inicio")
-st.page_link("pages/3_Grafica_de_barras.py", label="Siguiente ➡️")
+st.page_link("pages/page_1.py", label="⬅️ Regresar")
+st.page_link("pages/page_3.py", label="Siguiente ➡️")
