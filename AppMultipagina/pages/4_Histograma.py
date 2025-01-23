0301054598e7ca853e72babf12e2d684f0d51f4e
ValeriaPeniche <122946520+ValeriaPeniche@users.noreply.github.com>
2025-01-23 13:19:14 -0500

    Update 4_Histograma.py

--- AppMultipagina/pages/4_Histograma.py
+++ AppMultipagina/pages/4_Histograma.py
@@ -2,16 +2,10 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 
+st.subheader("Histograma de frecuencias")
 df = pd.read_csv("../Datos/quality_life_2020.csv", sep=";")
 
-###################################
-# Histograma de frecuencias
-##################################
-
-st.subheader("Histograma de frecuencias")
-
 # Agregar un selector de enteros para elegir el número de bines
-
 num_bins = st.sidebar.slider("Selecciona el número de bines:", min_value=5, max_value=15, value=8, step=1)
 
 histogram = alt.Chart(df).mark_bar().encode(
@@ -24,3 +18,6 @@ histogram = alt.Chart(df).mark_bar().encode(
 )
 
 st.altair_chart(histogram, use_container_width=True)
+
+st.page_link("pages/3_Grafica_de_barras.py", label="⬅️ Regresar")
+st.page_link("pages/5_Grafica_de_caja.py", label="Siguiente ➡️")
