2408ebbe6831714b4c2bac8d621ce551da10879d
ValeriaPeniche <122946520+ValeriaPeniche@users.noreply.github.com>
2025-01-30 13:08:36 -0500

    Update main.py

--- Visualización de datos OCDE/main.py
+++ Visualización de datos OCDE/main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Esperanza de Vida - OCDE",
+    page_icon="🌍",
+    layout="wide"
+)
+
+st.title("📊 Visualización de la Esperanza de Vida en Países de la OCDE")
+
+st.sidebar.success("Selecciona una página arriba para comenzar.")
 
