e339ac0d93f3cd71885d6f7784adc66d21d5dac3
ValeriaPeniche <122946520+ValeriaPeniche@users.noreply.github.com>
2025-01-30 14:38:40 -0500

    Update 4_Visualizacion2.py

--- Visualización de datos OCDE/pages/4_Visualizacion2.py
+++ Visualización de datos OCDE/pages/4_Visualizacion2.py
@@ -2,27 +2,30 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar datos
 @st.cache_data
 def load_data():
-    df = pd.read_csv("Datos/OECD.HEALTH_STATUS.csv")
+    df = pd.read_csv("Visualización_de_datos_OCDE/Datos/OECD.HEALTH_STATUS.csv")
     return df
 
 df = load_data()
 
 st.title("📈 Series de Tiempo")
 
-# Selector de países
+# países miembros de la OCDE
+ocde_paises = [
+    "AUS", "AUT", "BEL", "CAN", "CHL", "CZE", "DNK", "EST", 
+    "FIN", "FRA", "DEU", "GRC", "HUN", "ISL", "IRL", "ISR", "ITA", 
+    "JPN", "MEX", "NLD", "NZL", "NOR", "POL", "PRT", "SVK", 
+    "SVN", "ESP", "SWE", "CHE", "GBR", "USA"
+]
+
 paises_disponibles = sorted(df["REF_AREA"].unique())
 paises_seleccionados = st.multiselect("Selecciona países:", paises_disponibles, default=[paises_disponibles[0]])
+genero = st.radio("Seleccionar datos por género:", ["Total", "Male", "Female"])
 
-# Selector de género
-genero = st.radio("Seleccionar datos por género:", ["Total", "Males", "Females"])
+df_filtrado = df[(df["REF_AREA"].isin(paises_seleccionados)) & (df["Sex"] == genero if genero != "Total" else True)]
 
-# Filtrar datos
-df_filtrado = df[(df["REF_AREA"].isin(paises_seleccionados)) & (df["SEX"] == genero)]
-
-# Graficar
+# Grafica
 fig, ax = plt.subplots(figsize=(10, 6))
 for pais in paises_seleccionados:
     datos_pais = df_filtrado[df_filtrado["REF_AREA"] == pais]
